537322c094d930ffdb87884fe4a2a984a2d18a23
Ivan Lee <ivnyle@gmail.com>
2022-06-20 20:24:11 -0700

    save

--- run_astar.sh
+++ run_astar.sh
@@ -1,33 +1,24 @@
-export TRANSFORMERS_CACHE='/trunk/ivanlee/hf_cache'
-export HF_DATASETS_CACHE='/trunk/ivanlee/hf_cache'
-export CUDA_VISIBLE_DEVICES=1
-
 MODEL='facebook/blenderbot-400M-distill'
-# MODEL='facebook/blenderbot-1B-distill'
 
-# DATA="blended_skill_talk"
+# Which dataset to generate over
+DATA="blended_skill_talk"
 # DATA='meena'
-# DATA='persona' # maybe get rid of first couple utts
+# DATA='persona'
 # DATA='empath'
 # DATA='wow'
-DATA='otters'
 
+# How to delimit the dialogue history. We use 'tab' in the experiments.
 DELIMIT='tab'
 # DELIMIT='raw'
 
-DEBUG=True
-# DEBUG=False
+# DEBUG=True
+DEBUG=False
 
-STR=15
+# Annealing schedule hyperparameters
+STR=15 # lambda in the paper
 C=0
 
-# for DATA in blended_skill_talk
-# # for DATA in meena persona empath wow
-# do
-# for STR in 1 5 10 20 40 80 160
-# do
-# for C in 0 #1 2 3 4
-# do
+# Runs HeLo
 python astar.py \
     --model $MODEL \
     --dataset $DATA \
@@ -47,93 +38,64 @@ python astar.py \
     --cosine_mode 'extend' \
     --do_bst_truncate True \
 
-# done
-# done
-# done
+# Runs beam search baseline
+python astar.py \
+    --model $MODEL \
+    --dataset $DATA \
+    --seed 0 \
+    --debug $DEBUG \
+    --split "test" \
+    --do_astar False \
+    --do_cosine False \
+    --do_prompt False \
+    --output_dir gen \
+    --start_idx 0 \
+    --delimiter $DELIMIT \
+    --astar_top_k 40 \
+    --astar_strength $STR \
+    --c $C \
+    --max_samples 250 \
+    --cosine_mode 'extend' \
+    --do_bst_truncate True \
 
-# for DATA in meena persona empath wow
-# do
-# python astar.py \
-#     --model $MODEL \
-#     --dataset $DATA \
-#     --seed 0 \
-#     --debug False \
-#     --split "test" \
-#     --do_astar False \
-#     --do_cosine False \
-#     --do_prompt True \
-#     --output_dir gen \
-#     --start_idx 0 \
-#     --delimiter $DELIMIT \
-#     --astar_top_k 40 \
-#     --astar_strength 10 \
-#     --c 2 \
-#     --max_samples 50 \
+# Runs prefix + beam baseline
+python astar.py \
+    --model $MODEL \
+    --dataset $DATA \
+    --seed 0 \
+    --debug $DEBUG \
+    --split "test" \
+    --do_astar False \
+    --do_cosine False \
+    --do_prompt True \
+    --output_dir gen \
+    --start_idx 0 \
+    --delimiter $DELIMIT \
+    --astar_top_k 40 \
+    --astar_strength $STR \
+    --c $C \
+    --max_samples 250 \
+    --cosine_mode 'extend' \
+    --do_bst_truncate True \
 
-# done
+# Runs CoSim baseline
+python astar.py \
+    --model $MODEL \
+    --dataset $DATA \
+    --seed 0 \
+    --debug $DEBUG \
+    --split "test" \
+    --do_astar False \
+    --do_cosine True \
+    --do_prompt False \
+    --output_dir gen \
+    --start_idx 0 \
+    --delimiter $DELIMIT \
+    --astar_top_k 40 \
+    --astar_strength $STR \
+    --c $C \
+    --max_samples 250 \
+    --cosine_mode 'extend' \
+    --do_bst_truncate True \
 
-# python astar.py \
-#     --model "facebook/blenderbot-400M-distill" \
-#     --dataset "blended_skill_talk" \
-#     --seed 0 \
-#     --debug False \
-#     --split "test" \
-#     --do_astar False \
-#     --do_cosine False \
-#     --do_prompt True \
-#     --output_dir gen \
-#     --start_idx 0 \
-#     --max_samples 50 \
-#     --delimiter 'tab' \
-#     --astar_top_k 40 \
-#     --astar_strength 10 \
-#     --c 2 \
-
-# python astar.py \
-#     --model "facebook/blenderbot-400M-distill" \
-#     --dataset "blended_skill_talk" \
-#     --seed 0 \
-#     --debug True \
-#     --split "test" \
-#     --do_astar False \
-#     --do_cosine False \
-#     --do_prompt False \
-#     --astar_strength 10 \
-#     --astar_top_k 40 \
-#     --output_dir gen \
-#     --start_idx 5 \
-#     --max_samples 50 \
-#     --delimiter 'raw' \
-
-# python astar.py \
-#     --model $MODEL \
-#     --dataset $DATA \
-#     --seed 0 \
-#     --debug True \
-#     --split "test" \
-#     --do_astar False \
-#     --do_cosine False \
-#     --do_prompt False \
-#     --astar_strength 10 \
-#     --astar_top_k 40 \
-#     --output_dir gen \
-#     --start_idx 5 \
-#     --max_samples 50 \
-
-# for TOPK in 40 20 10 60
-# do
-# for STR in 5 10 15 20
-# do
-# python astar.py \
-#     --model "facebook/blenderbot-400M-distill" \
-#     --dataset "blended_skill_talk" \
-#     --seed 0 \
-#     --debug False \
-#     --split "test" \
-#     --astar_strength $STR \
-#     --astar_top_k $TOPK \
-#     --output_dir gen \
-#     --max_samples 50 \
-
-# done
-# done
+    
